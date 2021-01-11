65c1ad736078a54ce9e04b83d6284cc8de4705c0
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-01-11 08:21:22 -0400

    Updated test google login

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -1,18 +1,18 @@
-// module.exports = {
-//     '@tags': ['login'],
-//     '@disabled': false,
-//     'Log in into Trello with username and password'(browser) {
-//         const page = browser.page.LoginPage();
-//         page
-//             .navigate()
-//             .setTxtValue('@txtUserName', 'osalamar@gmail.com')
-//             .setTxtValue('@txtPassword', 'ramalaso')
-//             .logIn();
-//         browser
-//             .assert.urlContains('boards', 'Verify that we are in home page url')
-//             .assert.visible('._3qwe2tMMFonNvf', 'Verify that web navigator is at Home')
-//             .saveScreenshot('tests_output/login.png')
-//     }}
+module.exports = {
+    '@tags': ['login'],
+    '@disabled': false,
+    'Log in into Trello with username and password'(browser) {
+        const page = browser.page.LoginPage();
+        page
+            .navigate()
+            .setTxtValue('@txtUserName', 'osalamar@gmail.com')
+            .setTxtValue('@txtPassword', 'ramalaso')
+            .logIn();
+        browser
+            .assert.urlContains('boards', 'Verify that we are in home page url')
+            .assert.visible('._3qwe2tMMFonNvf', 'Verify that web navigator is at Home')
+            .saveScreenshot('tests_output/login.png')
+    }}
 module.exports = {
     '@tags': ['login'],
     '@disabled': false,
