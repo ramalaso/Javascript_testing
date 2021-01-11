de7413dd8117f43fc773c48b161f4a78b0e2e5ca
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-01-11 09:53:45 -0400

    updated package.json and loginTest.js

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
-
 module.exports = {
     '@tags': ['login'],
     '@disabled': false,
     'Log in into Trello with username and password'(browser) {
         const page = browser.page.LoginPage();
+        require('dotenv').config()
         page
             .navigate()
             .setTxtValue('@txtUserName', process.env.GOOGLE_USERNAME)
@@ -15,22 +14,22 @@ module.exports = {
             .assert.visible('._3qwe2tMMFonNvf', 'Verify that web navigator is at Home')
             .saveScreenshot('tests_output/login.png')
     }}
-module.exports = {
-    '@tags': ['login'],
-    '@disabled': false,
-    'Log in into Trello with google account'(browser) {
-        const page = browser.page.LoginPage();
-        page
-            .navigate()
-            .googleBtn()
-            .setTxtValue('@txtGoogleUserName', 'ramalaso45')
-            .pressEnter()
-            .setTxtValue('@txtGooglePassword', 'ramalaso')
-            .pressEnter()
+// module.exports = {
+//     '@tags': ['login'],
+//     '@disabled': false,
+//     'Log in into Trello with google account'(browser) {
+//         const page = browser.page.LoginPage();
+//         page
+//             .navigate()
+//             .googleBtn()
+//             .setTxtValue('@txtGoogleUserName', 'ramalaso45')
+//             .pressEnter()
+//             .setTxtValue('@txtGooglePassword', 'ramalaso')
+//             .pressEnter()
 
-        browser
-            .assert.urlContains('boards', 'Verify that we are in home page url')
-            .assert.visible('._3qwe2tMMFonNvf', 'Verify that web navigator is at Home')
-            .saveScreenshot('tests_output/login.png');
-    }
-}
+//         browser
+//             .assert.urlContains('boards', 'Verify that we are in home page url')
+//             .assert.visible('._3qwe2tMMFonNvf', 'Verify that web navigator is at Home')
+//             .saveScreenshot('tests_output/login.png');
+//     }
+// }
